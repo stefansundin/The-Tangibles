793ccfa22ea598082df442c0e1d0a1851bcf63a0
Jonas Brood <jonas.brood@gmail.com>
2012-10-16 15:04:59 +0200

    Updating API strings

--- webrtc/socket.js
+++ webrtc/socket.js
@@ -25,7 +25,8 @@ var API_MESSAGE_BROADCAST = "msgbroadcast";
 var API_CORNERS = "corners";
 var API_CORNERS_BROADCAST = "cornersbroadcast";
 
-var API_SET_NAME = "setname";
+var API_NAME_SET = "setname";
+var API_NAME_CHANGE = "changename";
 
 var ROOM_PUBLIC = "public";
 var ROOM_PRIVATE = "private";
