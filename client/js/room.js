aaf8e9aff13f907d9dc8393142aea482bf764234
kalleo <karl.ohman@gmail.com>
2012-10-27 13:07:09 +0200

    Found a socket that was not replaced.

--- client/js/room.js
+++ client/js/room.js
@@ -245,7 +245,7 @@ function writeMessageToChat(message) {
 	var room = window.location.hash.slice(1);
 	var color = "#"+((1<<24)*Math.random()|0).toString(16);
 
-	rtc._socket.send(JSON.stringify({
+	parent.socket.send(JSON.stringify({
 		"eventName": "chat_msg",
 		"data": {
 		"messages": message,
