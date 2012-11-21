391ea2bcd7cb382ccfc2f41fa530f08a31b506ca
Nicklas Nyström <nirivonsiri@gmail.com>
2012-11-21 15:22:20 +0100

    Changed the logo.

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ app.get('/img/ltu_logo.jpg', function(req, res) {
     res.sendfile(__dirname + '/client/img/ltu_logo.jpg');
 });
 
+app.get('/img/ltu_logo_white.png', function(req, res) {
+        res.sendfile(__dirname + '/client/img/ltu_logo_white.png');
+        });
+
 app.get('/img/qr1012.png', function(req, res) {
     res.sendfile(__dirname + '/client/img/qr1012.png');
 });
